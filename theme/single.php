54e3a9c8bb07c54b4143d33afb4990d061764a41
Viktor Nagy <viktor.nagy@balabit.com>
2017-03-09 21:03:07 +0100

    renamed single.php to singular.php

--- theme/single.php
+++
@@ -1,6 +0,0 @@
-<h2 class="major"><?php the_title(); ?></h2>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-the_content();
-endwhile; else: ?>
-<p>Sorry, no posts matched your criteria.</p>
-<?php endif; ?>
---
+++ theme/singular.php
@@ -0,0 +1,10 @@
+<?php
+// Start the loop.
+while ( have_posts() ) : the_post();
+
+    the_title( '<h2 class="major">', '</h2>' );
+    the_content();
+
+// End the loop.
+endwhile;
+?>

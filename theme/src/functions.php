181a9dd44cf92d509307b96c5d1fa0bfccea081f
Viktor Nagy <viktor.nagy@balabit.com>
2017-03-25 22:46:12 +0100

    improved main menu links

--- theme/src/functions.php
+++ theme/src/functions.php
@@ -83,7 +83,7 @@ function html5blank_nav()
         'after'           => '',
         'link_before'     => '',
         'link_after'      => '',
-        'items_wrap'      => '<ul>%3$s</ul>',
+        'items_wrap'      => '<ul class="actions">%3$s</ul>',
         'depth'           => 0,
         'walker'          => ''
         )

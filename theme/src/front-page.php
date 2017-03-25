2aa4a7c02809d65d4efb5454334a03b0748f41ce
Viktor Nagy <viktor.nagy@balabit.com>
2017-03-25 22:03:45 +0100

    Fixing frontpage image

--- theme/src/front-page.php
+++ theme/src/front-page.php
@@ -2,15 +2,15 @@
 
 	<main role="main" aria-label="Content">
 		<!-- section -->
-		<section class="wrapper style2 align-center">
-        <div class="inner">
+		<section class="banner style2">
+        <div class="content">
 
 			<h1><?php the_title(); ?></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" class="content" ?>>
+			<article id="post-<?php the_ID(); ?>">
 
 			// check if the post has a Post Thumbnail assigned to it.
 				<?php the_content(); ?>
@@ -22,11 +22,6 @@
 				<?php edit_post_link(); ?>
 
 			</article>
-			<?php if ( has_post_thumbnail() ) { ?>
-				<div class="image">
-					<?php the_post_thumbnail(); ?>
-				</div>
-			<?php } ?>
 			<!-- /article -->
 
 		<?php endwhile; ?>
@@ -44,6 +39,11 @@
 		<?php endif; ?>
 
 		</div>
+		<?php if ( has_post_thumbnail() ) { ?>
+			<div class="image">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		<?php } ?>
 		</section>
 		<!-- /section -->
 	</main>
